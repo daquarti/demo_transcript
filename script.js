80bc38dc5e4bfd0d7e1b2f0e3130b4db73103216
Doc <daquarti@hotmail.com>
2023-11-17 22:40:09 -0300

    text area

--- script.js
+++ script.js
@@ -3,14 +3,7 @@ function printLikePrinter(text, elementId, speed) {
     let interval = setInterval(function() {
         if (i < text.length) {
             let char = text.charAt(i);
-            if (char === '\n') {
-                // Inserta un elemento <br> para el salto de línea
-                document.getElementById(elementId).appendChild(document.createElement('br'));
-            } else {
-                // Crea un nodo de texto para el carácter y lo agrega
-                let textNode = document.createTextNode(char);
-                document.getElementById(elementId).appendChild(textNode);
-            }
+            document.getElementById(elementId).value += char;
             i++;
         } else {
             clearInterval(interval);
@@ -18,20 +11,14 @@ function printLikePrinter(text, elementId, speed) {
     }, speed);
 }
 
-// Texto a imprimir, con saltos de línea
-const text = "Motivos de consulta: Tos con flema amarillenta y con sangre\n" +
-             "\n" + // Salto de línea adicional
-             "Antecedentes: Tabaquista, Padre fallecido por cáncer de pulmón\n" +
-             "\n" + // Salto de línea adicional
-             "Evolución: Paciente presenta tos intensa con flema amarillenta y rasgos de sangre, pérdida de peso de 10 kilos en 6 meses, antecedente familiar de cáncer de pulmón\n" +
-             "\n" + // Salto de línea adicional
+const text = "Motivos de consulta: Tos con flema amarillenta y con sangre\n\n" +
+             "Antecedentes: Tabaquista, Padre fallecido por cáncer de pulmón\n\n" +
+             "Evolución: Paciente presenta tos intensa con flema amarillenta y rasgos de sangre, pérdida de peso de 10 kilos en 6 meses, antecedente familiar de cáncer de pulmón\n\n" +
              "Plan terapéutico: Realizar radiografía de tórax y prueba de sangre\n" +
              "para confirmar diagnóstico y determinar tratamiento adecuado";
 
-
-// Añadir un listener al botón para iniciar la impresión del texto
 const button = document.getElementById('print-button');
 button.addEventListener('click', function() {
-    document.getElementById('text-area').textContent = ''; // Limpiar el área de texto antes de imprimir
+    document.getElementById('text-area').value = ''; // Limpia el área de texto antes de imprimir
     printLikePrinter(text, 'text-area', 100); // Ajusta la velocidad de impresión según sea necesario
 });
